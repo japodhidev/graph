069746a7c79e00fe70d6597fa046776baff90020
Finch Ochieng <japodhidev@gmail.com>
2019-02-16 16:04:53 +0000

    added api routes

--- app.py
+++ app.py
@@ -20,13 +20,11 @@ yaxis = []
 xaxis = []
 zaxis = []
 
-
-
 # Dash Graph's x, y axes
-X = deque(maxlen=20)
-X.append(1)
-Y = deque(maxlen=20)
-Y.append(1)
+# X = deque(maxlen=20)
+# X.append(1)
+# Y = deque(maxlen=20)
+# Y.append(1)
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -50,12 +48,45 @@ def pi(name=None):
         y_lst = str(yaxis)
         return render_template('index.html', xa=x_lst, ya=y_lst)
 
+@app.route('/api', methods=['POST'])
+def rasp(name=None):
+    # Get data sent through from the client via a POST request
+    pi_data = request.json
+    # Populate variables with the data received
+    append_list(pi_data)
+    
+    # Return a json response
+    return jsonify(pi_data)
+
+@app.route('/api')
+def rasp_homepage(name=None):
+    # Typecast to string and send the values alongside the template
+    x_lst = str(xaxis)
+    y_lst = str(yaxis)
+    return render_template('index.html', xa=x_list, ya=y_lst)
+
+@app.route('/api/xaxis', methods=['POST'])
+def x_axis(name=None):
+    # Return y-axis values as a json response
+    xax = jsonify(xaxis)
+    
+    return xax
+
+@app.route('/api/yaxis', methods=['POST'])
+def y_axis(name=None):
+    # Return y-axis values as a json response
+    yax = jsonify(yaxis)
+    
+    return yax
+    
 def append_list(dq_x):
     # print(type(dq_x))
+    # Load data sent through the request
     dq_ = json.loads(dq_x)
+    # Loop counter for iterating through the values sent
     counter = 0
+    # Populate the values into coordinates
     for key, value in dq_.items():
-        
         if counter == 0:
             zaxis.append(value)
         elif counter == 1:
@@ -66,6 +97,7 @@ def append_list(dq_x):
         counter = counter + 1
     print ("element: ", type(value))
     print(yaxis)
+    # Limit the length list to 10 elements long
     if (len(xaxis) == 10):
         xaxis.pop(0)
         yaxis.pop(0)
