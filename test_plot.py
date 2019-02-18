314da16a9b8ae095fd847173d557b4440ea35dae
japodhidev <japodhidev@gmail.com>
2019-02-18 08:16:56 +0300

    populate test

--- test_plot.py
+++ test_plot.py
@@ -3,12 +3,12 @@ import plotly.graph_objs as go
 import requests
 import json
 
-data = {'x': 0.46, 'y': 0.7, 'z': 0.4}
+data = {'x': 0.46, 'y': 0.7, 'z': 0.5}
 
 # for key, value in data.items():
 # 	print (key, value)
 js_dt = json.dumps(data)
-response = requests.post('http://127.0.0.1:8000/', 
+response = requests.post('https://sheltered-coast-93272.herokuapp.com/', 
 	json=js_dt)
 print ("js_dt: ", js_dt)
 print ("data: ", type(data))
