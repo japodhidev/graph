1095ff0176988950a9f767af1462fa622a37a671
japodhidev <japodhidev@gmail.com>
2019-02-18 08:28:05 +0300

    populate test

--- test_plot.py
+++ test_plot.py
@@ -3,7 +3,7 @@ import plotly.graph_objs as go
 import requests
 import json
 
-data = {'x': 0.46, 'y': 0.7, 'z': 0.5}
+data = {'x': 0.46, 'y': 0.47, 'z': 0.5}
 
 # for key, value in data.items():
 # 	print (key, value)
